fb9e5b7051e9a0c351b82eb288982b8bcfec40a4
Inas Hyatt <inas.raheema@gmail.com>
2016-10-24 12:00:57 -0700

    stuff added

---
+++ java_intro.java
@@ -0,0 +1,75 @@
+/** Intro to strings */
+public class YourName {
+	public static void main(String[] args) {
+
+		System.out.println("Inas");
+
+	}
+}
+
+/** intro to print ints */
+public class DataTypes {
+	public static void main(String[] args) {
+
+		System.out.println(6);
+
+	}
+}
+
+/*intro to booleans*/
+public class DataTypesB {
+	public static void main(String[] args) {
+
+		System.out.println(true);
+
+	}
+}
+
+/*Intro to char*/
+public class DataTypesC {
+	public static void main(String[] args) {
+
+		System.out.println('I');
+
+	}
+}
+
+/*Intro to variables*/
+public class Variables {
+	public static void main(String[] args) {
+
+		int myNumber = 42;
+		boolean isFun = true; 
+		char movieRating = 'A'; 
+
+	}
+}
+
+//Relational operators
+
+public class RelationalOperators {
+	public static void main(String[] args) {
+
+		System.out.println(4 > 8);
+
+	}
+}
+
+
+//Summary of learning
+public class Generalizations {
+	public static void main(String[] args) {
+		//comment
+
+		boolean isComplete = true;
+		int awesomeLevel = 121;
+		int epicLevel = awesomeLevel * 2;		
+		System.out.println(epicLevel);
+		
+
+	}
+}
+
+
+
+
